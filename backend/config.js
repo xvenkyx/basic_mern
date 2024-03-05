38795cb72457c3e92014cec215203271fc0e3335
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2024-03-05 07:57:32 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const MongoDBURL =
-  "mongodb+srv://root:root@book-store-mern.cqlyo3j.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "enter MONGO URL here";
